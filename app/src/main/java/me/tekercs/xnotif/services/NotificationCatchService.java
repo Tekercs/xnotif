f952e69c06c1730a2fe528f87911cd52069c0651
Sebestyen Bence <sebestyen.bence.125@gmail.com>
2016-12-19 16:39:45 +0000

    notification meta wrapper

--- app/src/main/java/me/tekercs/xnotif/services/NotificationCatchService.java
+++ app/src/main/java/me/tekercs/xnotif/services/NotificationCatchService.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 
+import me.tekercs.xnotif.entities.NotificationMeta;
+
 /**
  * Created by sebestyen on 18/12/16.
  */
@@ -15,11 +17,17 @@ public class NotificationCatchService extends NotificationListenerService
     {
         super.onNotificationPosted(sbn);
 
+        NotificationMeta notificationMeta = this.getNotificationMetaFrom(sbn);
+        System.out.println(notificationMeta.toString());
+    }
+
+    private NotificationMeta getNotificationMetaFrom(StatusBarNotification sbn)
+    {
         Bundle extras = sbn.getNotification().extras;
         String title = extras.getString("android.title");
         String text = extras.getCharSequence("android.text").toString();
 
-        System.out.println(title + "/" + text);
+        return new NotificationMeta(title, text);
     }
 
     @Override
